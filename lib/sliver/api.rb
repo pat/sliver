acac8a79a690c5c2313a49cbf15019d724223515
Pat Allan <pat@freelancing-gods.com>
2017-06-02 14:50:55 +1000

    No need for lambda, just return the 404 response directly.

--- lib/sliver/api.rb
+++ lib/sliver/api.rb
@@ -1,6 +1,5 @@
 class Sliver::API
-  NOT_FOUND_RESPONSE = [404, {'X-Cascade' => 'pass'}, ['Not Found']].freeze
-  NOT_FOUND = lambda { |environment| NOT_FOUND_RESPONSE }
+  NOT_FOUND = [404, {'X-Cascade' => 'pass'}, ['Not Found']].freeze
 
   def initialize(&block)
     @endpoints = Sliver::Endpoints.new
@@ -11,7 +10,7 @@ class Sliver::API
   def call(environment)
     endpoint = endpoints.find environment
 
-    endpoint.nil? ? NOT_FOUND.call(environment) : invoke(endpoint, environment)
+    endpoint.nil? ? NOT_FOUND : invoke(endpoint, environment)
   end
 
   def invoke(endpoint, environment)
